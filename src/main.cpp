ea96fbf74c4dfceb1135364039899484492105b7
Jaefae <Jaefae@icloud.com>
2026-03-09 12:05:18 -0400

    Main cli interaction loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "cli.h"
 #include <iostream>
+#include "parser.h"
 #define debug
 
 int main(int argc, char *argv[]) {
@@ -10,5 +11,19 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   std::cout << "[INFO] Configuration built." << std::endl;
-  return 0;
+  ParseResult res = parseFile(cfg);
+  for (auto& error: res.errors) {
+    std::cout << "[Warn] " << error.message <<
+      " (line " << error.lineNumber << ")" << std::endl;
+  }
+  if (saveFile(cfg, res)) {
+    std::cout << "[INFO] Cards output to " << absolute(cfg.outputPath).string() << ".\n";
+    return 0;
+  } else {
+    if (cfg.strictWarn) {
+      std::cout << "[ERROR] Cards not saved due to listed errors. (strict warn mode)" << std::endl;
+    }
+    std::cout << "[ERROR] Cards could not be saved to " << absolute(cfg.outputPath).string() << ".\n";
+    return 1;
+  }
 }
